4407029deddbb7bdb441a0bfbe59dc632595a077
michael <ei.dwaps@gmail.com>
2024-04-19 11:10:49 +0200

    add model bar

---
+++ database/models/bar.model.js
@@ -0,0 +1,31 @@
+const { define, DataTypes } = require("sequelize");
+
+const Bar = define("Bar", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tel: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = Bar;
