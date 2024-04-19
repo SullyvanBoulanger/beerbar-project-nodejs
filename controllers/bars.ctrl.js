f45574afb756c52ba406e9de34cce90deef5a243
michael <ei.dwaps@gmail.com>
2024-04-19 11:25:41 +0200

    create controllers

---
+++ controllers/bars.ctrl.js
@@ -0,0 +1,19 @@
+module.exports.getBars = (req, res) => {
+  res.status(200).json({});
+};
+
+module.exports.getBar = (req, res) => {
+  res.status(200).json({});
+};
+
+module.exports.deleteBar = (req, res) => {
+  res.status(200).json({});
+};
+
+module.exports.createBar = (req, res) => {
+  res.status(200).json({});
+};
+
+module.exports.updateBar = (req, res) => {
+  res.status(200).json({});
+};
--- routing/bars.routes.js
+++ routing/bars.routes.js
@@ -1,9 +1,17 @@
+const {
+  getBar,
+  getBars,
+  createBar,
+  updateBar,
+  deleteBar,
+} = require("../controllers/bars.ctrl");
+
 const router = require("express").Router();
 
-router.post("", () => {});
-router.put("bars/:id", () => {});
-router.delete(":id", () => {});
-router.get("bars", () => {});
-router.get("bars/:id", () => {});
+router.get("bars", getBars);
+router.get("bars/:id", getBar);
+router.post("", createBar);
+router.put("bars/:id", updateBar);
+router.delete(":id", deleteBar);
 
 module.exports = router;
