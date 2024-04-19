2c91130738ed42c467540d9a79e7c04936644d41
michael <ei.dwaps@gmail.com>
2024-04-19 22:36:05 +0200

    feat: PUT /bars/:id_bar => Modifier un bars(#3)

--- controllers/bar.ctrl.js
+++ controllers/bar.ctrl.js
@@ -26,6 +26,10 @@ module.exports.getBar = async (req, res) => {
   }
 };
 
+module.exports.createBar = (req, res) => {
+  res.status(200).json({});
+};
+
 module.exports.deleteBar = async (req, res) => {
   try {
     const { id } = req.params;
@@ -34,20 +38,37 @@ module.exports.deleteBar = async (req, res) => {
       return res.status(404).json({ error: "Bar introuvable." });
     }
     await bar.destroy();
+
     res.status(200).json({ message: "Bar supprimé avec succès." });
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        error: "Une erreur est survenue lors de la suppression du bar.",
-      });
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la suppression du bar.",
+    });
   }
 };
 
-module.exports.createBar = (req, res) => {
-  res.status(200).json({});
-};
+module.exports.updateBar = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, address, tel, email, description } = req.body;
 
-module.exports.updateBar = (req, res) => {
-  res.status(200).json({});
+    const bar = await Bar.findByPk(id);
+    if (!bar) {
+      return res.status(404).json({ error: "Bar introuvable." });
+    }
+
+    await bar.update({
+      name,
+      address,
+      tel,
+      email,
+      description,
+    });
+
+    res.status(200).json({ message: "Bar mis à jour avec succès." });
+  } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la mise à jour du bar.",
+    });
+  }
 };
