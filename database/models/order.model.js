d041ff5c8710b1cd7192c9b267ac1985225c270f
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-04-19 11:36:08 +0200

    feat: Add Order model

---
+++ database/models/order.model.js
@@ -0,0 +1,33 @@
+const { define, DataTypes } = require("sequelize");
+const Bar = require("./bar.model");
+
+const Order = define("Order", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+    validate: {
+      min: 0,
+    },
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+Order.belongsTo(Bar);
+
+module.exports = Order;
