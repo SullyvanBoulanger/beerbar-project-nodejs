f1b6ea65293b577caf5781e09e131b6124bafd56
michael <ei.dwaps@gmail.com>
2024-04-19 21:33:32 +0200

    feat: GET /bars => Liste des bars (#5)

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const express = require("express");
 const app = express();
 const router = require("./routing/index");
 
-app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(router);
 
 module.exports = app;
--- controllers/bar.ctrl.js
+++ controllers/bar.ctrl.js
@@ -1,5 +1,14 @@
-module.exports.getBars = (req, res) => {
-  res.status(200).json({});
+const Bar = require("../database/models/bar.model");
+
+module.exports.getBars = async (req, res) => {
+  try {
+    const bars = await Bar.findAll();
+    res.status(200).json(bars);
+  } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la récupération des bars.",
+    });
+  }
 };
 
 module.exports.getBar = (req, res) => {
