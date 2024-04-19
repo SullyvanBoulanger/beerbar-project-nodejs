f15cc244c9d7325c09dce373807fd4e7753e2d95
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-04-19 14:33:49 +0200

    fix: Fix routes of order

--- routing/order.routes.js
+++ routing/order.routes.js
@@ -8,10 +8,10 @@ const {
 
 const router = require("express").Router();
 
-router.post("bars/:id_bar/commandes", postOrder);
-router.put("commandes/:id_commande", putOrder);
-router.delete("commandes/:id_commande", deleteOrder);
-router.get("bars/:id_bar/commandes", getOrdersByBar);
-router.get("commandes/:id", getOrderById);
+router.post("/bars/:id_bar/commandes", postOrder);
+router.put("/commandes/:id_commande", putOrder);
+router.delete("/commandes/:id_commande", deleteOrder);
+router.get("/bars/:id_bar/commandes", getOrdersByBar);
+router.get("/commandes/:id", getOrderById);
 
 module.exports = router;
