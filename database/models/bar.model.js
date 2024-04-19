bbb31f592fa14bb674d92f26ccd90d5d3dbba7c9
michael <ei.dwaps@gmail.com>
2024-04-19 13:30:01 +0200

    validation on bar model

--- database/models/bar.model.js
+++ database/models/bar.model.js
@@ -17,10 +17,23 @@ const Bar = define("Bar", {
   tel: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isPhoneNumber(value) {
+        if (!/^0[1-9](\d{2}){4}$/.test(value)) {
+          throw new Error("Le format du numéro de téléphone est invalide.");
+        }
+      },
+    },
   },
   email: {
     type: DataTypes.STRING,
     allowNull: true,
+    unique: true,
+    validate: {
+      isEmail: {
+        msg: "L'adresse e-mail est invalide.",
+      },
+    },
   },
   description: {
     type: DataTypes.TEXT,
