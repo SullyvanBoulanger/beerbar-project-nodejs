7e484cc40b50dc4e83b5215e008cc1710da3120c
michael <ei.dwaps@gmail.com>
2024-04-19 12:41:46 +0200

    add ssl redirection

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 require("dotenv").config();
 require("./database/index");
+const express = require("express");
+const app = express();
+
+// DEV
+app.get("*", (req, res) => res.send("TEST"));
+
+module.exports = app;
