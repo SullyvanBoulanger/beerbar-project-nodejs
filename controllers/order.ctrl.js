1a412689b88d55059b87b1903caaebf5ff4830e1
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-04-22 10:59:42 +0200

    feat: Implement DELETE Order by id

--- controllers/order.ctrl.js
+++ controllers/order.ctrl.js
@@ -18,8 +18,20 @@ module.exports.putOrder = (req, res) => {
   res.status(200).json({});
 };
 
-module.exports.deleteOrder = (req, res) => {
-  res.status(200).json({});
+module.exports.deleteOrder = async (req, res) => {
+  try {
+    const { order_id } = req.params;
+    const order = await Order.findByPk(order_id);
+    if (!order) {
+      return res.status(404).json({ error: "Commande introuvable." });
+    }
+    await order.destroy();
+    res.status(200).json({ message: "Commande supprimée avec succès." });
+  } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la suppression de la commande.",
+    });
+  }
 };
 
 module.exports.getOrdersByBar = async (req, res) => {
