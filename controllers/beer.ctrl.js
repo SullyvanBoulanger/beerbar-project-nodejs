6793a21ced036e4486bd3726edef1d04b611618a
Abdelac <149737714+abdelac30@users.noreply.github.com>
2024-04-23 15:52:07 +0200

    Feat/update beer (#45)
    * AddBeer
    
    * updateBeer
    
    ---------
    
    Co-authored-by: Cornillon Michael <ei.dwaps@gmail.com>

--- controllers/beer.ctrl.js
+++ controllers/beer.ctrl.js
@@ -1,4 +1,7 @@
+const { response } = require("../app");
+const Bar = require("../database/models/bar.model");
 const Beer = require("../database/models/beer.model");
+const Beer = require("../models/beer.model");
 
 module.exports.getBeers = (req, res) => {
   res.status(200).json({ message: "Liste de toutes les beers" });
@@ -30,15 +33,47 @@ module.exports.deleteBeer = (req, res) => {
 };
 
 module.exports.createBeer = async (req, res) => {
-  await Beer.create(req.body);
+  const id_bar = req.params.id_bar;
+
+  try {
+    const bar = await Bar.findByPk(id_bar);
+    if (!bar) {
+      return res.status(404).json({
+        error: "le bar n'existe pas",
+      });
+    }
+
+    const beer = await Beer.create({
+      ...req.body,
+      bar_id: id_bar,
+    });
+    res.status(201).json(beer);
+  } catch (error) {
+    res.status(500).json({
+      error: "la biere n'a pas pu etre créée",
+    });
+  }
   res.status(200).json({ message: "Nouvelle beer créée avec succès" });
 };
 
-module.exports.updateBeer = (req, res) => {
+module.exports.updateBeer = async (req, res) => {
   const beer_id = req.params.beer_id;
-  res
-    .status(200)
-    .json({ message: `La beer avec l'ID ${beer_id} a été mise à jour` });
+
+  try {
+    const beer = await Beer.findByPk(beer_id);
+
+    if (!beer) {
+      return res.status(404).json({ error: "La bière n'existe pas" });
+    }
+
+    await beer.update(req.body);
+
+    res
+      .status(200)
+      .json({ message: `La bière avec l'ID ${beer_id} a été mise à jour` });
+  } catch (error) {
+    res.status(500).json({ error: "La mise à jour de la bière a échoué" });
+  }
 };
 
 module.exports.getAverageBeerDegrees = async (req, res) => {
