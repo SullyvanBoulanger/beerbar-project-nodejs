3cde16b8aecc75a858bb81197beafbf8e7b46368
MiL-Dev <mil.devacc@gmail.com>
2024-04-23 11:28:48 +0200

    feat: Implement GET Beer by id

--- controllers/beer.ctrl.js
+++ controllers/beer.ctrl.js
@@ -1,17 +1,32 @@
+const Beer = require("../database/models/beer.models");
+
 module.exports.getBeers = (req, res) => {
   res.status(200).json({ message: "Liste de toutes les beers" });
 };
 
-module.exports.getBiere = (req, res) => {
-  const beer_id = req.params.beer_id;
-  res.status(200).json({ message: `Détails de la bière avec l'ID ${beer_id}` });
+module.exports.getBeer = async (req, res) => {
+  try {
+    const { beer_id } = req.params; 
+    const beer = await Beer.findByPk(beer_id);
+
+    if(!beer) {
+      res.status(404).json({
+        error: "Commande introuvable."
+      });
+    }
+    res.status(200).json(beer);
+  } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la récupération de la commande."
+    })
+  }
 };
 
 module.exports.deleteBeer = (req, res) => {
   const beer_id = req.params.beer_id;
   res
     .status(200)
-    .json({ message: `La beer avec l'ID ${beer_id} a été supprimée` });
+    .json({ message: `La bière avec l'ID ${beer_id} a été supprimée` });
 };
 
 module.exports.createBeer = (req, res) => {
