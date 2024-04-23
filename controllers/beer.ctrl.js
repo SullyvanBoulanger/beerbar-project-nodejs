a8b98af28ddbf914c98e4639bd144d0350be3036
MiL-Dev <97902097+MiL-Dev@users.noreply.github.com>
2024-04-23 15:18:09 +0200

    feat: Implement GET Beer by id (#42)
    * feat: Implement GET Beer by id
    
    * Change message error to bière
    
    * fix: import beer controler to beer.model
    
    * Refacto: line 14 b to B

--- controllers/beer.ctrl.js
+++ controllers/beer.ctrl.js
@@ -1,21 +1,36 @@
+const Beer = require("../database/models/beer.model");
+
 module.exports.getBeers = (req, res) => {
   res.status(200).json({ message: "Liste de toutes les beers" });
 };
 
-module.exports.getBiere = (req, res) => {
-  const beer_id = req.params.beer_id;
-  res.status(200).json({ message: `Détails de la bière avec l'ID ${beer_id}` });
+module.exports.getBeer = async (req, res) => {
+  try {
+    const { beer_id } = req.params; 
+    const beer = await Beer.findByPk(beer_id);
+
+    if(!beer) {
+      res.status(404).json({
+        error: "Bière introuvable."
+      });
+    }
+    res.status(200).json(beer);
+  } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la récupération de la bière."
+    })
+  }
 };
 
 module.exports.deleteBeer = (req, res) => {
   const beer_id = req.params.beer_id;
   res
     .status(200)
-    .json({ message: `La beer avec l'ID ${beer_id} a été supprimée` });
+    .json({ message: `La bière avec l'ID ${beer_id} a été supprimée` });
 };
 
 module.exports.createBeer = (req, res) => {
-  res.status(200).json({ message: "Nouvelle beer créée avec succès" });
+  res.status(200).json({ message: "Nouvelle bière créée avec succès" });
 };
 
 module.exports.updateBeer = (req, res) => {
