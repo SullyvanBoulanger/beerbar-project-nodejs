9c960f65a8bb29f4cb49def43ce75ee6edcaeeda
abdelac <abdelac@hotmail.fr>
2024-04-24 16:07:34 +0200

    positiv-price-validator

--- routing/beer.routes.js
+++ routing/beer.routes.js
@@ -7,12 +7,22 @@ const {
   getAverageBeerDegrees,
 } = require("../controllers/beer.ctrl");
 const router = require("express").Router();
+const { validatePositivePrice } = require("../middlewares/beer.validator");
+const { catchError } = require("../middlewares/catchError");
 
 router.get("/bars/:bar_id/beers", getBeers);
 router.get("/beers/:beer_id", getBeer);
 router.get("/bars/:bar_id/degrees", getAverageBeerDegrees);
-router.post("/bars/:bar_id/beers", createBeer);
-router.put("/beers/:beer_id", updateBeer);
+router.post(
+  "/bars/:bar_id/beers",
+  [validatePositivePrice(), catchError],
+  createBeer
+);
+router.put(
+  "/beers/:beer_id",
+  [validatePositivePrice(), catchError],
+  updateBeer
+);
 router.delete("/beers/:beer_id", deleteBeer);
 
 module.exports = router;
