11f001339845919bc46b5c4cfc4de262cbf87c33
abdelac <abdelac@hotmail.fr>
2024-04-24 16:43:10 +0200

    positiv order price

--- middlewares/order.validator.js
+++ middlewares/order.validator.js
@@ -1,9 +1,9 @@
 const { body } = require("express-validator");
-const Status = [
-  "en cours",
-  "terminée",
-];
-module.exports.validateOrderStatus = () => body(
-  "status",
-  "Status doit être soit en cours ou terminée."
-).isIn(Status);
+const Status = ["en cours", "terminée"];
+module.exports.validateOrderStatus = () =>
+  body("status", "Status doit être soit en cours ou terminée.").isIn(Status);
+
+module.exports.validatePositiveOrderPrice = () =>
+  body("price", "Le prix de la commande n'est pas positif").isFloat({
+    min: 0,
+  });
--- routing/order.routes.js
+++ routing/order.routes.js
@@ -5,12 +5,23 @@ const {
   getOrdersByBar,
   getOrder,
 } = require("../controllers/order.ctrl");
-const { validateOrderStatus } = require("../middlewares/order.validator");
+const {
+  validateOrderStatus,
+  validatePositiveOrderPrice,
+} = require("../middlewares/order.validator");
 const { catchError } = require("../middlewares/catchError");
 const router = require("express").Router();
 
-router.post("/bars/:bar_id/orders", [validateOrderStatus(), catchError], createOrder);
-router.put("/orders/:order_id", [validateOrderStatus(), catchError], updateOrder);
+router.post(
+  "/bars/:bar_id/orders",
+  [validateOrderStatus(), validatePositiveOrderPrice(), catchError],
+  createOrder
+);
+router.put(
+  "/orders/:order_id",
+  [validateOrderStatus(), validatePositiveOrderPrice(), catchError],
+  updateOrder
+);
 router.delete("/orders/:order_id", deleteOrder);
 router.get("/bars/:bar_id/orders", getOrdersByBar);
 router.get("/orders/:order_id", getOrder);
