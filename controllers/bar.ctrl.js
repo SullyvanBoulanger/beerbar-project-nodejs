1b0079e5508e0a2c041e78d50e1109d58eb4abb0
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-04-25 10:01:46 +0200

    refacto: Remove useless console.error

--- controllers/bar.ctrl.js
+++ controllers/bar.ctrl.js
@@ -46,7 +46,6 @@ module.exports.deleteBar = async (req, res) => {
 
     res.status(200).json({ message: "Bar supprimé avec succès." });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ error: Errors.BAR_DELETE });
   }
 };
